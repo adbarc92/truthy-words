b2c30c3dd716ba96aba007bd7afdd521654f5cf6
adbarc92 <adbarclay92@gmail.com>
2021-10-05 14:31:15 -0600

    Adds Eslint and dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    'jest/globals': true,
+    browser: true,
+    commonjs: true,
+    es2021: true,
+		node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    camelcase: 1,
+    'arrow-body-style': 0,
+    'no-nested-ternary': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'comma-dangle': 1,
+    'no-unused-vars': 1,
+    'no-void': 0,
+    'no-tabs': 0,
+    '@typescript-eslint/no-var-requires': 1,
+  },
+  settings: {},
+};
