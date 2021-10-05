a334a268ba4280b1f4ce672d1843cca108fd11d5
adbarc92 <adbarclay92@gmail.com>
2021-10-05 13:45:08 -0600

    Builds successfully

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'index'),
+  entry: path.resolve(__dirname, 'src', 'index'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin()],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: { sourceMap: false },
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.ejs' }),
+    new CompressionPlugin({
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 0,
+      minRatio: 0.8,
+    }),
+  ],
   module: {
     rules: [
       {
@@ -24,5 +42,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
   },
 };
