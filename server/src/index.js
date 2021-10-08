13d91a962764dc29fae21483df9fdef9e95d6050
adbarc92 <adbarclay92@gmail.com>
2021-10-08 12:50:21 -0600

    Server proxies requests correctly

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
 
-const apiKey = require('../config');
+const { dictApiKey } = require('../config');
 
-const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=${apiKey}`;
+const baseUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json`;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -14,11 +15,29 @@ app.use('*.js' || '*.jsx', (req, res, next) => {
   res.set('Content-Encoding', 'gzip');
   next();
 });
+
 app.use(express.json());
 app.use(morgan('common'));
 app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-/* TODO: ADD PROXYING ROUTE HERE */
+app.all('/api/*', (req, res) => {
+  const url = `${baseUrl}/${req.url.slice(4)}?key=${dictApiKey}`;
+  console.log('url:', url);
+  axios({
+    method: req.method,
+    url,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    data: req.body
+  })
+    .then((apiRes) => {
+      res.status(apiRes.status).send(apiRes.data);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
