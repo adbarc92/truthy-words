f3a2f75c44e8ea859911d58ea82be21a11f6e273
adbarc92 <adbarclay92@gmail.com>
2021-10-08 12:30:34 -0600

    Adds function to validate word response

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,6 +1,6 @@
 // CREDIT: https://www.freecodecamp.org/news/javascript-debounce-example/
 
-export const createDebounceFn = (fn, delay): (() => void) => {
+export const createDebounceFn = (fn, delay = 500): (() => void) => {
   let timer;
   return function (...args) {
     clearTimeout(timer);
@@ -8,4 +8,14 @@ export const createDebounceFn = (fn, delay): (() => void) => {
       fn.apply(this, args);
     }, delay);
   };
+};
+
+export const getWordValidity = (data): boolean => {
+  if (
+    !data.fl ||
+    !['adverb', 'adjective', 'noun', 'verb'].includes(data.fl)
+  ) {
+    return false;
+  }
+  return true;
 };
